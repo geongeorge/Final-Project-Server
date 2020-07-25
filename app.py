a9219bd94b5c4ff89cb21ab860708f88e3711453
Geon George <geongeorge98@gmail.com>
2020-07-25 20:32:56 +0530

    Uploads picture api

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask, escape, request
+import werkzeug
 
 app = Flask(__name__)
+
+UPLOADS = 'uploads/'
 
 @app.route('/')
 def hello():
     name = request.args.get("name", "World")
     return f'Hello, {escape(name)}!'
+
+@app.route('/image', methods = ['POST'])
+def processImage():
+    imagefile = request.files['image']
+    filename = werkzeug.utils.secure_filename(imagefile.filename)
+    print("\nReceived image File name : " + imagefile.filename)
+    imagefile.save(UPLOADS+filename)
+    return 'Success !'
